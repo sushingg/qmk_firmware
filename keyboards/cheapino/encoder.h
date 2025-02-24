ebfcd53dac04107a02369692b55a8b32cac51c9d
sushingg <t.shingg@gmail.com>
2025-02-25 00:57:25 +0700

    add keyboard cheapino

---
+++ keyboards/cheapino/encoder.h
@@ -0,0 +1,9 @@
+//
+// Created by Thomas Haukland on 25/03/2023.
+//
+
+void fix_encoder_action(matrix_row_t current_matrix[]);
+
+bool encoder_click_user(void);
+
+bool encoder_turned_user(bool clockwise);
